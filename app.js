d4a1071b640119a5072c044725b015a4da444a89
superb-shan <106537831+superb-shan@users.noreply.github.com>
2024-02-08 23:25:27 +0530

    sonar

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express")
 const collection = require("./mongo")
 const cors = require("cors")
-const app = express()
+const app = express() 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
